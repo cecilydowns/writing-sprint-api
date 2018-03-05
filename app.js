fde83793da5b37ff2aff21fc9ae69702cba56662
Cecily <cecily.downs@gmail.com>
2018-03-04 17:08:02 -0800

    Set up JWT

--- app.js
+++ app.js
@@ -6,12 +6,39 @@ const MongoStore = require('connect-mongo')(session);
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const passportJWT = require('passport-jwt')
 const promisify = require('es6-promisify');
 
 const expressValidator = require('express-validator');
 
-const routes = require('./routes/index');
+
 const errorHandlers = require('./handlers/errorHandlers');
+
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+
+const User = mongoose.model('User')
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),
+  secretOrKey: process.env.SECRET
+}
+
+const routes = require('./routes/index');
+
+
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+  console.log('payload received', jwt_payload)
+  const user = User.findOne({ 'id': jwt_payload.id })
+  if(user){
+    next(null, user)
+  } else {
+    next(null, false)
+  }
+})
+
+passport.use(strategy)
 
 const app = express();
 
@@ -41,7 +68,8 @@ app.use(passport.session());
 //     next();
 //   });
 
-app.use('/', routes);
+app.use('/', routes)
+// app.use('/', passport.authenticate('jwt', {session: false}), routes);
 
 // If that above routes didnt work, we 404 them and forward to error handler
 app.use(errorHandlers.notFound);
