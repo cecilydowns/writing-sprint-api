5067fa3aff03f6fd5da012f2bbd01ba740847e4f
Cecily <cecily.downs@gmail.com>
2018-03-04 17:06:27 -0800

    Add createSprint and getSprints to sprints controller

--- controllers/sprintController.js
+++ controllers/sprintController.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose')
+const Sprint = mongoose.model('Sprint')
+
 exports.homePage = (req, res) => {
     const message = { message: 'Looks like I work' }
     res.json(message)
 };
   
+exports.createSprint = async (req, res) => {
+    
+    const sprint = await (new Sprint(req.body)).save()
+    res.json(req.body)
+}
+
+exports.getSprints = async (req, res) => {
+    const sprints = await Sprint.find()
+    res.json(sprints)
+}
