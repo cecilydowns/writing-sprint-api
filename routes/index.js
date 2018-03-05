9a04d4869abffb294a043dc584abc4c2fc2d5763
Cecily <cecily.downs@gmail.com>
2018-03-04 16:49:57 -0800

    Add additional routes for sprints and auth

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
+var passport = require("passport");
 
 const sprintController = require('../controllers/sprintController')
-
+const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', catchErrors(sprintController.homePage));
+
+router.get('/sprints', passport.authenticate('jwt', { session: false }), catchErrors(sprintController.getSprints))
+
+router.post('/sprints', passport.authenticate('jwt', { session: false }), catchErrors(sprintController.createSprint))
+
+router.post('/register', catchErrors(userController.register))
+
+router.post('/login', catchErrors(authController.login));
+
 
 module.exports = router;
