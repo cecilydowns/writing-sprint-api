ca336da6685078c096c1dc69bb0bfae037ff95c6
Cecily <cecily.downs@gmail.com>
2018-03-06 13:54:15 -0800

    Use req.login in auth controller login

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,15 +7,23 @@ const bcrypt = require('bcrypt')
 exports.login = async (req, res, next) => {
     const user = await User.findOne({ 'email': req.body.email })
 
+    // passport.authenticate
+
     if(!user) {
         res.status(401).json({message:"no such user found"});
     }
 
     bcrypt.compare(req.body.password, user.password, (err, bcres) => {
         if(bcres === true){
-            const payload = {id: user.id};
-            const token = jwt.sign(payload, process.env.SECRET);
-            res.json({message: "ok", token: token});
+            req.logIn(user, (err) => {
+                if(err) { 
+                    return next(err)
+                } else {
+                    const payload = {id: user.id};
+                    const token = jwt.sign(payload, process.env.SECRET);
+                    res.json({message: "ok", token: token});
+                }
+            })
         } else {
             res.status(401).json({message:"Wrong password."});
         }
