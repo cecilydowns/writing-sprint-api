3c12df42e10d2a422baac21db33fa2a40c112961
Cecily <cecily.downs@gmail.com>
2018-03-11 14:21:31 -0700

    Add project routes and google/fb auth routes to routes file

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,32 @@ const { catchErrors } = require('../handlers/errorHandlers')
 
 router.get('/', catchErrors(sprintController.homePage));
 
-router.get('/sprints', passport.authenticate('jwt', { session: false }), catchErrors(sprintController.getSprints))
+router.get('/projects', passport.authenticate('jwt', { session: false }), catchErrors(projectController.getProjects))
+router.post('/projects', passport.authenticate('jwt', { session: false }), catchErrors(projectController.createProject))
 
+router.get('/sprints', passport.authenticate('jwt', { session: false }), catchErrors(sprintController.getSprints))
 router.post('/sprints', passport.authenticate('jwt', { session: false }), catchErrors(sprintController.createSprint))
 
 router.post('/register', catchErrors(userController.register))
-
 router.post('/login', catchErrors(authController.login));
+
+
+
+// Set up Facebook auth routes
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/auth/facebook' }),
+  // Redirect user back to the mobile app using Linking with a custom protocol OAuthLogin
+  (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
+
+// Set up Google auth routes
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/auth/google' }),
+  (req, res) => res.redirect('OAuthLogin://login?user=' + JSON.stringify(req.user)));
+
 
 
 module.exports = router;
